b471ec465938cf0b3965d44780455aacf7784579
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-11-11 16:38:15 +0100

    Url extraction from a page

--- scan.py
+++ scan.py
@@ -3,7 +3,17 @@
 # Developer:	Praveen Kumar Pendyala
 # Created:	18/11/2014
 
+import bs4
 import urllib
+from bs4 import BeautifulSoup
 
-mListUrl = 'http://moodle.net/sites/'
-print urllib.urlopen(mListUrl).read()
+html = urllib.urlopen('http://moodle.net/sites/').read()
+html = html[html.index("<ul style='list-style:none;list-spacing:10px;'>"):]
+
+soup = BeautifulSoup(html)
+for site in soup.find_all('li'):
+	name = site.a
+	if(name):
+		link =  name.get('href')
+		if(link):
+			print link
